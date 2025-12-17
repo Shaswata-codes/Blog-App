e9ad7b9e1d28163f9f7f140fc08e179e943e5f77
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2025-12-17 17:47:40 +0530

    Blog Card Modified

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -3,6 +3,7 @@ import {blog_data, blogCategories} from '../assets/assets'
 import {motion} from "motion/react"
 import BlogCard from './BlogCard';
 
+
 const BlogList = () => {
 
   const [menu, setMenu] = useState("All");
@@ -26,11 +27,11 @@ const BlogList = () => {
             </div>
           ))}
         </div>
-        <div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8
+        mb-24 mx-8 sm:mx-16 xl:mx-40'>
           {blog_data.filter((blog)=>menu === "All"?
             true :  blog.category === menu
-          )
-          .map((blog)=><BlogCard key={blog._id} blog={blog}/>)}
+          ).map((blog)=><BlogCard key={blog._id} blog={blog}/>)}
         </div>
     </div>
   )
