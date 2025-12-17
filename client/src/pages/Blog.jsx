3aeb78b8eefb6cc96f49e0f2dcf2115f62f8ee6e
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2025-12-18 02:22:56 +0530

    Blog Page stared

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { blog_data } from '../assets/assets';
+import { assets, blog_data } from '../assets/assets';
+import Navbar from '../components/Navbar';
+import Moment from 'moment';
 
 const Blog = () => {
   const {id} = useParams();
   const [data, setData] = useState(null);
-  const fetchBlogData = async()=>{
-    const data = blog_data(item=>item._id === id);
-    setData(data);
-  }
+  const fetchBlogData = () => {
+  const blog = blog_data.find(item => item._id === id);
+  setData(blog);
+};
+
   useEffect(()=>{
     fetchBlogData();
   },[])
   return data?(
-    <div>
-      Blog
+    <div className='relative'>
+      <img src={assets.gradientBackground} className='absolute -top-50 -z-1 opacity-50'/>
+      <Navbar/>
+      <div>
+        <p>Published On {Moment(data.createdAt).format('')}</p>
+      </div>
+      <div></div>
+
     </div>
   )
   :<div>Loading...</div>
