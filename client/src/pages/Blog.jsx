bb3ca5161d7ae8525069b34fc3bd438bda9f56e2
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2025-12-22 20:44:04 +0530

    Blog Page updated

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -19,8 +19,11 @@ const Blog = () => {
     <div className='relative'>
       <img src={assets.gradientBackground} className='absolute -top-50 -z-1 opacity-50'/>
       <Navbar/>
-      <div>
-        <p>Published On {Moment(data.createdAt).format('')}</p>
+      <div className='text-center mt-20 text-gray-600'>
+        <p className='text-primary py-4 font-medium'>Published On {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+        <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
+        <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+        <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>Shasssss</p>
       </div>
       <div></div>
 
