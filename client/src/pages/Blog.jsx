8a0a15cf5ea473b5da876a44de6656c350950a1c
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2025-12-30 23:59:18 +0530

    Comments Added

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { assets, blog_data } from '../assets/assets';
+import { assets, blog_data, comments_data } from '../assets/assets';
 import Navbar from '../components/Navbar';
 import Moment from 'moment';
 
 const Blog = () => {
   const {id} = useParams();
   const [data, setData] = useState(null);
+  const [comments, setComments] = useState([]);
   const fetchBlogData = () => {
-  const blog = blog_data.find(item => item._id === id);
-  setData(blog);
-};
+    const blog = blog_data.find(item => item._id === id);
+    setData(blog);
+  };
+
+  const fetchComments = () =>{
+    setComments(comments_data);
+  }
 
   useEffect(()=>{
     fetchBlogData();
+    fetchComments();
   },[])
   return data?(
     <div className='relative'>
@@ -26,8 +32,29 @@ const Blog = () => {
         <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>Shasssss</p>
       </div>
       <div className='mx-5 max-w-5xl md:auto my-10 mt-6'>
-        <img src={data.image} className='rounded-3xl mb-5'/>
-        <div></div>
+        <img src={data.image} className='rounded-3xl mb-5 w-full translate-x-[100px]'/>
+        <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html: data.description}}></div>
+        <div className='mt-14 mb-10 max-w-3xl mx-auto'>
+          <p className='font-semibold mb-4'> Comments ({comments.length}) </p>
+          <div className='flex flex-col gap-4'>
+            {comments.map((item, index) => (
+              <div
+                key={index}
+                className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600'
+              >
+                <div className='flex items-center gap-2 mb-2'>
+                  <img src={assets.user_icon} className='w-6' />
+                  <p className='font-medium'>{item.name}</p>
+                </div>
+
+                <p className='text-sm max-w-md ml-8'>{item.content}</p>
+                <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>
+                  {Moment(item.createdAt).fromNow()}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
 
       </div>
 
