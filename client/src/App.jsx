45fde8244b9587e16e8fcbb0244a9b77686fd2d3
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2025-12-31 13:15:18 +0530

    Sidebar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Login from './components/admin/Login'
+import Dashboard from './pages/admin/Dashboard'
+import AddBlog from './pages/admin/AddBlog'
+import ListBlog from './pages/admin/ListBlog'
+import Layout from './pages/admin/Layout'
+import Comments from './pages/admin/Comments'
 
 const App = () => {
   return (
@@ -9,6 +15,11 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/blog/:id' element={<Blog/>} />
+        <Route path='/admin' element={<Layout/>} />
+          <Route index element={<Dashboard/>} />
+          <Route path='add-blog' element={<AddBlog/>} />
+          <Route path='list-blog' element={<ListBlog/>} />
+          <Route path='comments' element={<Comments/>} />
       </Routes>
     </div>
   )
