061617aeb8e8edd052d37c8cdfe9a8447ce38db6
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2026-01-09 13:32:48 +0530

    Login API working

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import adminRouter from './routes/adminRoutes.js';
 
 const app = express();
 
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
+app.use('/api/admin', adminRouter);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
